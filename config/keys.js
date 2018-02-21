0c85ef0f7533448a505dad55cba818e93ea12c77
anbe6083 <anbe6083@colorado.edu>
2018-02-21 12:49:42 -0800

    Fixed keys

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 //determines if we're in production or development
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dev');
-} else {
   module.exports = require('./prod');
+} else {
+  module.exports = require('./dev');
 }
