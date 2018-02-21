e60edcbdc5686caa5c8b59d7d32c980f361b2c3d
anbe6083 <anbe6083@colorado.edu>
2018-02-21 10:47:06 -0800

    Added correct redirect URI for google auth

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
 require('./routes/authRoutes')(app);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.google_web.client_id,
+      clientSecret: keys.google_web.client_secret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,6 @@
 const passport = require('passport');
 
 module.exports = app => {
-  app.get('/auth/google', (req, res) => {
-    res.send('youre at /auth/google/callback');
-  });
-
-  app.get('/auth/google/callback', (req, res) => {
-    res.send('youre at /auth/google/callback');
-  });
-
   app.get('/api/logout', (req, res) => {
     res.send('youre at /api/logout');
   });
