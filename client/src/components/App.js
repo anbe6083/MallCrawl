3e27feb59f7d99e962b687feaaabca13b33c2b72
anbe6083 <anbe6083@colorado.edu>
2018-02-21 18:47:32 -0800

    Added header and css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const App = () => {
-  return <div>Test</div>;
+const Dashboard = () => {
+  return (
+    <div>
+      <h2> Dashboard</h2>
+    </div>
+  );
 };
+const SurveyNew = () => {
+  return (
+    <div>
+      <h2> SurveyNew</h2>
+    </div>
+  );
+};
+const Landing = () => {
+  return (
+    <div>
+      <h2> Landing</h2>
+    </div>
+  );
+};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/dashboard" component={Dashboard} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
-export default App;
+export default connect(null, actions)(App);
---
+++ client/src/components/Header.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect, MapStateToProps } from 'react-redux';
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="left brand-logo">
+            MallCrawl
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+export default connect(mapStateToProps)(Header);
