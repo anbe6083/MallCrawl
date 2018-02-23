fdea0afd3298d2864e08a578402b6dc2d6c0989f
anbe6083 <anbe6083@colorado.edu>
2018-02-22 21:56:06 -0800

    Deleted res.businesses

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -13,7 +13,7 @@ export default class Dashboard extends Component {
     this.callApi('/api/yelp')
       .then(res => {
         this.setState({
-          businesses: res.businesses
+          businesses: res
         });
       })
       .catch(err => console.log(err));
