4fcb1232471b544ace744531ec25a046d3565acb
anbe6083 <anbe6083@colorado.edu>
2018-02-23 10:50:24 -0800

    Added dummy value

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,7 +5,48 @@ export default class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      businesses: []
+      businesses: [
+        {
+          id: 'shooters-bar-and-billiards-santa-clarita',
+          name: 'Shooters Bar & Billiards',
+          image_url:
+            'https://s3-media2.fl.yelpcdn.com/bphoto/Uesb4vZv7oU7bwiH4WjC8Q/o.jpg',
+          is_closed: false,
+          url:
+            'https://www.yelp.com/biz/shooters-bar-and-billiards-santa-clarita?adjust_creative=WHRowCjXn_uWBF6FCBiYOw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=WHRowCjXn_uWBF6FCBiYOw',
+          review_count: 106,
+          categories: [
+            {
+              alias: 'poolhalls',
+              title: 'Pool Halls'
+            },
+            {
+              alias: 'sportsbars',
+              title: 'Sports Bars'
+            }
+          ],
+          rating: 4,
+          coordinates: {
+            latitude: 34.3776228562467,
+            longitude: -118.562951087952
+          },
+          transactions: [],
+          price: '$',
+          location: {
+            address1: '24450 Lyons Ave',
+            address2: '',
+            address3: '',
+            city: 'Santa Clarita',
+            zip_code: '91321',
+            country: 'US',
+            state: 'CA',
+            display_address: ['24450 Lyons Ave', 'Santa Clarita, CA 91321']
+          },
+          phone: '+16612875888',
+          display_phone: '(661) 287-5888',
+          distance: 6192.698805304425
+        }
+      ]
     };
   }
 
@@ -20,6 +61,7 @@ export default class Dashboard extends Component {
     const res = axios.get('/api/yelp').then(res => {
       var businessArr = this.state.businesses.slice();
       res.data.map(business => {
+        console.log(business);
         businessArr.push(business);
       });
       this.setState({
@@ -37,7 +79,6 @@ export default class Dashboard extends Component {
   };
 
   render() {
-    console.log(this.state);
     return (
       <div>
         {this.state.businesses.map(business => {
