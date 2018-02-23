b36afb322317c453ec1378f17cf67c859f6a4b3e
anbe6083 <anbe6083@colorado.edu>
2018-02-22 18:06:16 -0800

    Added code to make sure express serves up production assets

--- index.js
+++ index.js
@@ -48,5 +48,18 @@ let businesses = client
 
 require('./routes/authRoutes')(app);
 
+if (process.end.NODE_ENV === 'production') {
+  //Make sure express will serve up production assets
+  //like our main.js/main.css file
+  app.use(express.static('client/build'));
+
+  //Express will serve up the index.html file if
+  //it doesnt recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
